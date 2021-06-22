b87b7e1ccb97e84d4fcba91652cf77581c8f8302
ytrsoft <1654486219@qq.com>
2021-06-22 08:54:01 +0800

    move to maven

---
+++ src/test/java/com/ytrsoft/AppTest.java
@@ -0,0 +1,14 @@
+package com.ytrsoft;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
